754032cd82f39ea29016074d2d2462f0fc711347
[wanglugao] <[]>
2020-12-15 10:26:19 +0800

    1.将apkRename改回子模块

--- apkrename-plugin/src/main/groovy/com/apkrename/plugin/ApkRenamePlugin.groovy
+++ apkrename-plugin/src/main/groovy/com/apkrename/plugin/ApkRenamePlugin.groovy
@@ -41,15 +41,28 @@ public class ApkRenamePlugin implements Plugin<Project> {
 //                }
 //            }
 
-            project.android.applicationVariants.all { variant ->
-                variant.outputs.each { output ->
-                    File file = output.outputFile
-                    println(file)
-                    //https://blog.csdn.net/u014300915/article/details/78355420
-                    output.outputFile = new File(file.getParent(),System.currentTimeMillis()+"_"+file.getName())
-//                    output.outputFileName = System.currentTimeMillis()+"_"+output.outputFileName
-//                    output.outputFile = new File("\", nameMap(file.getName()))
-                }
+//            project.android.applicationVariants.all { variant ->
+//                variant.outputs.each { output ->
+//                    File file = output.outputFile
+//                    println(file)
+//                    //https://blog.csdn.net/u014300915/article/details/78355420
+//                    output.outputFile = new File(file.getParent(),System.currentTimeMillis()+"_"+file.getName())
+////                    output.outputFileName = System.currentTimeMillis()+"_"+output.outputFileName
+////                    output.outputFile = new File("\", nameMap(file.getName()))
+//                }
+//            }
+        }
+
+        // If you use each() to iterate through the variant objects,
+// you need to start using all(). That's because each() iterates
+// through only the objects that already exist during configuration time—
+// but those object don't exist at configuration time with the new model.
+// However, all() adapts to the new model by picking up object as they are
+// added during execution.
+        project.android.applicationVariants.all { variant ->
+            variant.outputs.all {
+                println("${variant.name}-${variant.versionName}.apk")
+                outputFileName = "${variant.name}-${variant.versionName}.apk"
             }
         }
     }
